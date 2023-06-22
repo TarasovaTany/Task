3fee4f45ea7c740e08ca325aad0ce365173ab45a
TarasovaTany <umnova.panther@yandex.ru>
2023-06-22 13:10:20 +0500

    'HomeWork19.3.3'

--- Task19.3.3/Task.py
+++ Task19.3.3/Task.py
@@ -5,38 +5,47 @@ status = 'available'
 
 res = requests.get(f'https://petstore.swagger.io/v2/pet/findByStatus?status={status}', headers={'accept': 'application/json'})
 
-# Вывод кода ответа запроса
-print(res.status_code)
-
-# Создание нового питомца
-headers = {'accept': 'application/json', 'Content-Type': 'application/json'}
-
-data = {"id": 1686629964416770296, "category": {"id": 0, "name": "string"},
-        "name": "Vasya", "photoUrls": ["string"], "tags": [{"id": 0, "name": "string"}],
-        "status": "available"}
-
-res = requests.post('https://petstore.swagger.io/v2/pet', headers=headers, json=data)
-
-# Вывод кода ответа запроса и данных о созданном питомце
+# Код ответа запроса и все питомцы
 print(res.status_code)
 print(res.json())
 
-# Изменение данных о питомце
+# Новый питомец
+headers = {'accept': 'application/json',
+           'Content-Type': 'application/json'}
+
+data = {"id": 1686629964416770296,
+        "category": {"id": 0, "name": "string"},
+        "name": "Vasya", "photoUrls": ["string"],
+        "tags": [{"id": 0, "name": "string"}],
+        "status": "available"
+        }
+
+res = requests.post('https://petstore.swagger.io/v2/pet',
+                    headers=headers, json=data)
+
+# Код ответа запроса и данные о созданном питомце
+print(res.status_code)
+print(res.json())
+
+# Изменим данные о питомце
 headers = {'accept': 'application/json', 'Content-Type': 'application/json'}
 
-data = {"id": 1686629964416770296, "category": {"id": 0, "name": "string"},
-        "name": "VaNya", "photoUrls": "string", "tags": {"id": 0, "name": "string"},
-        "status": "available"}
+data = {"id": 1686629964416770296,
+        "category": {"id": 0, "name": "string"},
+        "name": "VaNya",
+        "photoUrls": "string",
+        "tags": {"id": 0, "name": "string"},
+        "status": "available"
+        }
 
-# Удаление созданного питомца
+# Удалим созданного питомца
 res = requests.delete(f'https://petstore.swagger.io/v2/pet/{1686629964416770296}',
                       headers={'accept': 'application/json'})
 
-# Вывод кода ответа запроса и данных об удаленном питомце
+# Код ответа запроса и данные об удаленном питомце
 print(res.status_code)
 print(res.json())
 
-# Запросить всех зверят в available-статусе
 status = 'available'
 
 res = requests.get(f'https://petstore.swagger.io/v2/pet/findByStatus?status={status}',
@@ -44,3 +53,4 @@ res = requests.get(f'https://petstore.swagger.io/v2/pet/findByStatus?status={sta
 
 # Вывод кода ответа запроса
 print(res.status_code)
+print(res.json())
